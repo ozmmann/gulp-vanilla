f471e7759b0ee802ab8f528e244592813d044aa6
Student <student@lan.itstep.kiev.ua>
2017-08-30 20:24:14 +0300

    includer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
+var includer = require('gulp-htmlincluder');
  
 gulp.task('cssmin', function () {
     gulp.src('dev/css/**/*.css')
         .pipe(cssmin())
         .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('htmlIncluder', function() {
+    gulp.src('dev/**/*.html')
+    	.pipe(includer())
+        .pipe(gulp.dest('build/'));
 });
